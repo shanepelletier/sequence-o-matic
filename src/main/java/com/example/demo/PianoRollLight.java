c91beeda7c783c33d8cb32225ef16ec49e062573
Shane Pelletier <lotios611@gmail.com>
2022-03-16 02:06:12 -0300

    Add note values to piano keys, changed author to my normal GitHub instead of an old class-specific one.

--- src/main/java/com/example/demo/PianoRollLight.java
+++ src/main/java/com/example/demo/PianoRollLight.java
@@ -1,15 +1,17 @@
 package com.example.demo;
 
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextAlignment;
 
 public class PianoRollLight extends PianoRoll {
     private final Canvas canvas;
 
-    private final String[] KEY_NAMES = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "A"};
+    private final String[] KEY_NAMES = {"A", "G", "F#", "F", "E", "D#", "D", "C#", "C", "B", "A#", "A"};
     Rectangle[] keys;
 
     public PianoRollLight() {
@@ -29,14 +31,17 @@ public class PianoRollLight extends PianoRoll {
         gc.setFill(Color.WHEAT);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        gc.setFill(Color.WHITE);
         gc.setStroke(Color.BLACK);
+        gc.setTextBaseline(VPos.CENTER);
+        gc.setTextAlign(TextAlignment.CENTER);
         final double RECTANGLE_HEIGHT = canvas.getHeight() / 12;
         for (int i = 0; i < 12; ++i) {
+            gc.setFill(Color.WHITE);
             keys[i] = new Rectangle(0, i * RECTANGLE_HEIGHT, 40, RECTANGLE_HEIGHT);
             gc.fillRect(0, i * RECTANGLE_HEIGHT, 40, i * RECTANGLE_HEIGHT + RECTANGLE_HEIGHT);
             gc.strokeRect(0, i * RECTANGLE_HEIGHT, 40, i * RECTANGLE_HEIGHT + RECTANGLE_HEIGHT);
-
+            gc.setFill(Color.BLACK);
+            gc.fillText(KEY_NAMES[i], 20, i * RECTANGLE_HEIGHT + RECTANGLE_HEIGHT / 2);
         }
     }
 
